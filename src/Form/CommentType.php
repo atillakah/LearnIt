285d66c94b0dccac9d3f1fcc4be66ac924628410
atilla kahraman <atilla.kahraman.pro@gmail.com>
2021-02-04 00:15:52 +0100

    changing comment form from wysiwyg into textarea

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Comment;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +15,8 @@ class CommentType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('content', CKEditorType::class, array(
-                'config' => array(
-                    'entities' => false
-                )
-            ))
+            ->add('content', TextareaType::class 
+            )
             // ->add('createdAt')
             // ->add('updatedAt')
            // ->add('lesson')
